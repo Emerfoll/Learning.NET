3b9427f05d0f9f35328e720a0b01d0eda2a43e17
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-10-27 13:42:24 -0500

    Hello World app.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
