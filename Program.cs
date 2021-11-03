c05e68eb3aabe392acfff1fb82b3d6ab5612f04e
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-11-03 11:27:10 -0500

    learning about Methods/functions

--- Program.cs
+++ Program.cs
@@ -8,21 +8,41 @@ namespace Learning.NET
         {
             // Lets make a MadLib
 
-            string color, pluralNoun, celebrity;
+            // string color, pluralNoun, celebrity;
 
-            Console.Write("Enter a color: ");
-            color = Console.ReadLine();
+            // Console.Write("Enter a color: ");
+            // color = Console.ReadLine();
 
-            Console.Write("Enter a plural noun: ");
-            pluralNoun = Console.ReadLine();
+            // Console.Write("Enter a plural noun: ");
+            // pluralNoun = Console.ReadLine();
 
-            Console.Write("Enter a celebrity name: ");
-            celebrity = Console.ReadLine();
+            // Console.Write("Enter a celebrity name: ");
+            // celebrity = Console.ReadLine();
 
-            Console.WriteLine("Roses are " + color);
-            Console.WriteLine(pluralNoun + " are blue");
-            Console.WriteLine("I love " + celebrity);
+            // Console.WriteLine("Roses are " + color);
+            // Console.WriteLine(pluralNoun + " are blue");
+            // Console.WriteLine("I love " + celebrity);
+
+
+            // learning arrays
+
+            // int[] luckyNumbers = {11, 13, 19, 42};
+            
+            // Console.WriteLine(luckyNumbers[3]);
+            
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            sayHello(name);
 
         }
+
+        static void sayHello(string name)
+        {
+            Console.WriteLine("Hello " + name + "!");
+
+        }
+
+
     }
 }
