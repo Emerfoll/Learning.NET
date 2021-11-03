495fe9c53f1f509f049e2f5a178c7e4d6f0e5b8b
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-11-03 10:41:59 -0500

    making a madlib game.

--- Program.cs
+++ Program.cs
@@ -6,12 +6,22 @@ namespace Learning.NET
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter another number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
+            // Lets make a MadLib
 
-            Console.WriteLine(num1 + num2);
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity name: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I love " + celebrity);
 
         }
     }
